3b4f8e6cc02eca8ec2b4a3f0107a812a7987d2aa
Vivien <vivien.richaud@gmail;com>
2018-09-27 18:12:50 +0200

    ajout prompt

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,36 @@
+/*
+
 var monTitre = document.querySelector('h1');
 monTitre.textContent = 'Hello world!';
+*/
+
+
+/*
 
 document.querySelector('html').onclick = function() {
     alert('Aïe, arrêtez de cliquer !!');
+}
+
+*/
+
+var monBouton = document.querySelector('button');
+var monTitre = document.querySelector('h1');
+
+function définirNomUtilisateur() {
+  var monNom = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', monNom);
+  monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+
+
+
+if(!localStorage.getItem('nom')) {
+  définirNomUtilisateur();
+} else {
+  let nomEnregistré = localStorage.getItem('nom');
+  monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
+
+  monBouton.onclick = function() {
+  définirNomUtilisateur();
+}
 }
